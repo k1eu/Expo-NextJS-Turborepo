c9ca3add0c1bcb56cb79f2ff7a5774f56a54d70a
Tomasz Kielar <git@k1eu.dev>
2022-06-30 03:04:04 +0200

    build: configure expo webpack to support dynamic libraries

--- apps/mobile/webpack.config.js
+++ apps/mobile/webpack.config.js
@@ -1,16 +1,25 @@
-const createExpoWebpackConfigAsync = require('@expo/webpack-config');
+const createExpoWebpackConfigAsync = require("@expo/webpack-config");
+
+const rootPackage = require("./package.json");
+
+const dependencies = {
+  ...rootPackage.dependencies,
+  ...rootPackage.devDependencies,
+};
+
+const usedDeps = Object.keys(dependencies).filter(
+  (dep) => dependencies[dep] === "*"
+);
 
 module.exports = async function (env, argv) {
   const config = await createExpoWebpackConfigAsync(
     {
       ...env,
       babel: {
-        dangerouslyAddModulePathsToTranspile: [
-          'ui',
-        ]
-      }
+        dangerouslyAddModulePathsToTranspile: usedDeps,
+      },
     },
     argv
   );
   return config;
-}
+};

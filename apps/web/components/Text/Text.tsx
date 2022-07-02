2df7e14d42d08e263d5957b18afee83263e7e6d6
bartoszboruta <bboruta.dev@gmail.com>
2022-07-02 02:32:42 +0200

    chore: setup tests in nextjs app with nativebase

--- apps/web/components/Text/Text.tsx
+++ apps/web/components/Text/Text.tsx
@@ -4,8 +4,12 @@ import { Button } from "ui";
 import { Button as NativeBaseButton } from "native-base";
 
 const Text = () => {
-  // return <Button title="Test Text" />;
-  return <NativeBaseButton>NativeBaseButton</NativeBaseButton>;
+  return (
+    <>
+      <Button title="Test Text" />;
+      <NativeBaseButton>NativeBaseButton</NativeBaseButton>;
+    </>
+  );
 };
 
 export default Text;

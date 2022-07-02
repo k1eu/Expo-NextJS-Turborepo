b8bbdc798ee82f7ed86dc5a84863e09038edc8ad
bartoszboruta <bboruta.dev@gmail.com>
2022-07-02 03:06:13 +0200

    feat: add reanimated to mobile

--- apps/mobile/jest-setup.js
+++ apps/mobile/jest-setup.js
@@ -1,4 +1,15 @@
-jest.mock('@react-navigation/native/lib/commonjs/useLinking.native', () => ({
+global.__reanimatedWorkletInit = () => {};
+
+jest.mock("react-native/Libraries/Animated/NativeAnimatedHelper");
+jest.mock("react-native-reanimated", () =>
+  require("react-native-reanimated/mock")
+);
+
+global.ReanimatedDataMock = {
+  now: () => 0,
+};
+
+jest.mock("@react-navigation/native/lib/commonjs/useLinking.native", () => ({
   default: () => ({ getInitialState: { then: jest.fn() } }),
   __esModule: true,
 }));

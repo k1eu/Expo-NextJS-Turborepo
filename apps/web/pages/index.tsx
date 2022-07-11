45428942c635211ef62354fb5d029376b158d9ea
Tomasz Kielar <git@k1eu.dev>
2022-07-12 01:08:49 +0200

    chore: refactor web tests & structure

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,15 +1,13 @@
 import { Button } from "ui";
 import { Button as NativeBaseButton } from "native-base";
 
-import Text from "../components/Text/Text";
+import Buttons from "../components/Buttons";
 
 export default function Web() {
   return (
     <div>
       <h1>Web</h1>
-      <Text />
-      <Button title="NextJS" />
-      <NativeBaseButton>NativeBaseButton</NativeBaseButton>
+      <Buttons />
     </div>
   );
 }
